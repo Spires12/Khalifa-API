1832c8e5cce3ce4a7401f048ebc21cd8bc0e07bd
spires12 <saviopires29@gmail.com>
2021-03-19 11:43:51 -0300

    feat: configuration rack-attack

--- api/config/application.rb
+++ api/config/application.rb
@@ -34,12 +34,13 @@ module Api
 
 
     config.middleware.insert_before 0, Rack::Cors do
-      allow do 
-        origin '*'
-        resource '*'
-          headers: :any, 
-          methods: %i(get post put patch delete options headers)
+      allow do
+        origins '*'
+        resource '*',
+          headers: :any,
+          methods: %i(get post put patch delete options head)
       end
-    end 
+    end
+    config.middleware.use Rack::Attack
   end
 end
