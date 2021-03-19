933a5accb9db272a1aeb78283c883a7bc8ba2823
spires12 <saviopires29@gmail.com>
2021-03-18 21:01:36 -0300

    feat: configuration cors api

--- api/config/application.rb
+++ api/config/application.rb
@@ -31,5 +31,15 @@ module Api
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do 
+        origin '*'
+        resource '*'
+          headers: :any, 
+          methods: %i(get post put patch delete options headers)
+      end
+    end 
   end
 end
